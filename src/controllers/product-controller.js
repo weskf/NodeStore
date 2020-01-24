6b9a0902a57b6c72460c473baab492fc14c32b48
Wescley Klein Frohlich <wescley.kf@gmail.com>
2020-01-24 19:22:00 -0300

    commit

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -21,19 +21,28 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id, 
-        item: req.body
-    });
+    Product
+        .findByIdAndUpdate(req.params.id, {
+            $set: {
+                title: req.body.title,
+                description: req.body.description,
+                price: req.body.price
+            }
+        }).then(data => {
+            res.status(201).send({ message: 'Produto atualizado com sucesso!'});
+        }).catch(e => {
+            res.status(400).send({ message: 'Falha ao atualizar o produto', data: e})
+        });
 };
 
 exports.delete = (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id, 
-        item: req.body
-    });
+    Product
+        .findOneAndRemove(req.params.id)
+        .then(data => {
+            res.status(201).send({ message: 'Produto removido com sucesso!'});
+        }).catch(e => {
+            res.status(400).send({ message: 'Falha ao remover o produto', data: e})
+        });
 };
 
 exports.get = (req, res, next) => {
